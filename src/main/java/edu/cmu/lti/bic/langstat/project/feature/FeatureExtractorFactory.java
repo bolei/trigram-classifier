a4a80bf7f67a6cd8095d8e51a092d0f0d1de35ad
bolei <bolei@cmu.edu>
2013-04-26 01:08:20 -0400

    fixed compiling erro

--- src/main/java/edu/cmu/lti/bic/langstat/project/feature/FeatureExtractorFactory.java
+++ src/main/java/edu/cmu/lti/bic/langstat/project/feature/FeatureExtractorFactory.java
@@ -2,9 +2,7 @@ package edu.cmu.lti.bic.langstat.project.feature;
 
 public class FeatureExtractorFactory {
 	public static FeatureExtractor getFeatureExtractor(String name) {
-		if (name.equalsIgnoreCase("TestFeatureExtractor")) {
-			return new TestFeatureExtractor();
-		} else if (name.equalsIgnoreCase("MophoFeatureExtractor")) {
+		if (name.equalsIgnoreCase("MophoFeatureExtractor")) {
 			return new MophoFeatureExtractor();
 		}
 		return null;
